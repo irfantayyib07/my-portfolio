a0eba80cada4b4ba4006bca934a40d2baa0b349b
Irfan <irfantayyib07@gmail.com>
2025-06-12 02:43:02 +0500

    refactor: update project grid layout to dynamically adjust rows based on project data length

--- components/projects.tsx
+++ components/projects.tsx
@@ -14,7 +14,7 @@ export default function Projects() {
    ref={ref}
    id="projects" className="scroll-mt-28 mb-28">
    <SectionHeading>My projects</SectionHeading>
-   <div className="grid grid-rows-5">
+   <div className="grid" style={{ gridTemplateRows: `repeat(${PROJECTS_DATA.length}, minmax(0, 1fr))` }}>
     {PROJECTS_DATA.map((project, index) => (
      <React.Fragment key={index}>
       <Project {...project} />
